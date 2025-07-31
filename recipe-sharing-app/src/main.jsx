4c520a7d80845f062011ea1e4b31b324a0bc4819
Nour Khattab <nour.khattab90@gmail.com>
2025-07-31 22:12:33 +0300

    Update main.jsx

--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -1,28 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import RecipeDetails from './components/RecipeDetails'
-import EditRecipeForm from './components/EditRecipeForm'
 import './index.css'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/recipe/:id',
-    element: <RecipeDetails />,
-  },
-  {
-    path: '/edit/:id',
-    element: <EditRecipeForm />,
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )

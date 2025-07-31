68910fbba123824123c22e5142ffcf7d6273750d
Nour Khattab <nour.khattab90@gmail.com>
2025-07-31 11:18:56 +0300

    Add files via upload

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import WelcomeMessage from './components/WelcomeMessage';
+
+function App() {
+  return (
+    <>
+      <WelcomeMessage />
+      {/* The rest of your existing App content */}
+    </>
+  )
+}
+
+export default App

3146ee76ca6202ee0a41a1c5fcd579af53b743a7
Nour Khattab <nour.khattab90@gmail.com>
2025-08-15 13:26:13 +0300

    Add files via upload

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="p-8 text-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Recipe Sharing Platform
+      </h1>
+      <p className="mt-4 text-gray-600">
+        Welcome to our recipe sharing community!
+      </p>
+    </div>
+  );
+}
+
+export default App;

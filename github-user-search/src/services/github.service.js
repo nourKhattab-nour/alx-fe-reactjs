8c4a82aadbb8c02a8c91bf9486e76e1f52bbdf39
Nour Khattab <nour.khattab90@gmail.com>
2025-08-15 12:26:56 +0300

    Update github.service.js

--- github-user-search/src/services/github.service.js
+++ github-user-search/src/services/github.service.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
 export const fetchUserData = async (username) => {
-  try {
-    const response = await axios.get(`https://api.github.com/users/${username}`);
-    return response.data;
-  } catch (error) {
-    throw error; // Let component handle it
-  }
+  const response = await axios.get(`https://api.github.com/users/${username}`);
+  return response.data;
 };

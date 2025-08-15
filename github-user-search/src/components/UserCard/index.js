83f314c33cd01acb950a1bb41d472f17ae6be951
Nour Khattab <nour.khattab90@gmail.com>
2025-08-15 12:06:53 +0300

    Update index.js

--- github-user-search/src/components/UserCard/index.js
+++ github-user-search/src/components/UserCard/index.js
@@ -1,25 +1,31 @@
-import React from "react";
-import PropTypes from "prop-types";
+// src/components/UserCard/index.js
+import PropTypes from 'prop-types';
 
 const UserCard = ({ user }) => {
   return (
     <div className="user-card">
-      <img src={user.avatar_url} alt={user.login} className="user-avatar" />
-      <h2>{user.name || user.login}</h2>
-      <p>{user.bio || "No bio available"}</p>
-      <div className="user-stats">
-        <span>Followers: {user.followers}</span>
-        <span>Following: {user.following}</span>
-        <span>Repos: {user.public_repos}</span>
+      <img 
+        src={user.avatar_url} 
+        alt={user.login} 
+        className="user-avatar"
+      />
+      <div className="user-info">
+        <h2>{user.name || user.login}</h2>
+        {user.bio && <p className="user-bio">{user.bio}</p>}
+        <div className="user-stats">
+          <span>Followers: {user.followers}</span>
+          <span>Following: {user.following}</span>
+          <span>Repos: {user.public_repos}</span>
+        </div>
+        <a 
+          href={user.html_url} 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="profile-link"
+        >
+          View Profile
+        </a>
       </div>
-      <a
-        href={user.html_url}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="profile-link"
-      >
-        View Profile
-      </a>
     </div>
   );
 };
@@ -33,8 +39,8 @@ UserCard.propTypes = {
     followers: PropTypes.number.isRequired,
     following: PropTypes.number.isRequired,
     public_repos: PropTypes.number.isRequired,
-    html_url: PropTypes.string.isRequired,
-  }).isRequired,
+    html_url: PropTypes.string.isRequired
+  }).isRequired
 };
 
 export default UserCard;

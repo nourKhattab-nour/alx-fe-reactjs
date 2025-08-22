0e4a6b6b8407cd68d87f684e6a46a76a83c63e6c
Nour Khattab <nour.khattab90@gmail.com>
2025-08-22 20:17:39 +0300

    Update jest.config.js

--- react-todo/jest.config.js
+++ react-todo/jest.config.js
@@ -1,7 +1,11 @@
 export default {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
   moduleNameMapping: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1'
   },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  }
 };

849933c4502882b10cfb42c2a491ca1c4f786bde
Nour Khattab <nour.khattab90@gmail.com>
2025-08-22 14:51:11 +0300

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/blog" element={<Blog />} />
-            <Route path="/blog/:postId" element={<BlogPost />} />
+            <Route path="/blog/:id" element={<BlogPost />} /> {/* Updated to use :id */}
             <Route 
               path="/login" 
               element={
